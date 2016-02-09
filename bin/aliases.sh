ff9e353f65809b3d43aa91be4e47834db4414c61
Dan Schultz <schultz.t.dan@gmail.com>
2016-02-08 21:36:53 -0800

    Add alias to get local ip

--- bin/aliases.sh
+++ bin/aliases.sh
@@ -1,2 +1,11 @@
+# Usage:
+# $ dot-install
 alias dot-install="${DOTFILES}/script/install"
+
+# Usage:
+# $ ipconfig getifaddr en0
+alias ip="ipconfig getifaddr"
+
+# Usage
+# $ rasterize-html http://google.com ~/Desktop/google.jpg
 alias rasterize-html="phantomjs ${DOTFILES}/bin/rasterize-html.js"
