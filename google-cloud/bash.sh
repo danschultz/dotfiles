c40af2e5033369636d0da9e83517d3d9c1eaa2a4
Dan Schultz <schultz.t.dan@gmail.com>
2017-01-03 01:38:14 -0800

    Check if Google Cloud is installed before sourcing files

--- google-cloud/bash.sh
+++ google-cloud/bash.sh
@@ -1,5 +1,10 @@
-# The next line updates PATH for the Google Cloud SDK.
-source '/Users/dan/google-cloud-sdk/path.bash.inc'
+# Google Cloud isn't installed through Homebrew, check if it exists
+# before sourcing files.
+if test -d '~/google-cloud-sdk'
+then
+  # The next line updates PATH for the Google Cloud SDK.
+  source '/Users/dan/google-cloud-sdk/path.bash.inc'
 
-# The next line enables bash completion for gcloud.
-source '/Users/dan/google-cloud-sdk/completion.bash.inc'
+  # The next line enables bash completion for gcloud.
+  source '/Users/dan/google-cloud-sdk/completion.bash.inc'
+fi
