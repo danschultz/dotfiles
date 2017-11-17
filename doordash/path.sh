28a373db678af166dbc8063105007fc3a4631a55
Dan Schultz <schultz.t.dan@gmail.com>
2017-11-16 17:55:25 -0800

    Add Kubernetes default config

--- doordash/path.sh
+++ doordash/path.sh
@@ -1,3 +1,13 @@
 export DOORDASH=$HOME/Development/doordash
 
 export PATH=$PATH:$DOORDASH/doordash-containertools/bin
+
+# Kubernetes
+export KUBECONFIG=~/.kube/config.staging
+
+#export PIP_EXTRA_INDEX_URL=https://repo.fury.io/doordash/
+doordashrc='/usr/local/doordash/doordashrc'
+if test -f $doordashrc
+then
+  source $doordashrc
+fi
