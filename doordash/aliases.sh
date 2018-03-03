e7ba6f77d33dbeb655ec9fe2ba76c121df227150
Dan Schultz <schultz.t.dan@gmail.com>
2018-03-02 19:07:31 -0800

    Remove DoorDash

--- doordash/aliases.sh
+++
@@ -1,7 +0,0 @@
-# Usage:
-# $ remotepython -e sandbox0 -a doorstep-django
-alias remotepython="control venvcmd ddt remotepython"
-
-# Usage:
-# $ remoteshell -e sandbox0 -a doorstep-django
-alias remoteshell="control venvcmd ddt remoteshell"
