abc91e5ec55db29917a83a24f3a6a61b592d6b23
Dan Schultz <schultz.t.dan@gmail.com>
2018-07-06 11:15:46 -0500

    Try to include docker file

--- vertex/path.sh
+++ vertex/path.sh
@@ -1,1 +1,6 @@
-export SPEEDRACER_HOME=/Users/dan/Development/Vertex/
+export SPEEDRACER_HOME=$HOME/Development/Vertex/
+
+if test -f "$DOTFILES/vertex/docker.sh"
+then
+  source "$DOTFILES/vertex/docker.sh"
+fi
